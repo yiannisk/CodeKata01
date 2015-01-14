da0c17b0958616b2c6b1a749ab985b7c7d288f9b
Yiannis K <ikaradimas@hotmail.com>
2015-01-14 19:00:44 +0200

    Initial commit.

---
+++ Projects/Kl1nt.CodeKata.SupermarketPricing.Models/ProductOnOffer.cs
@@ -0,0 +1,8 @@
+﻿namespace Kl1nt.CodeKata.SupermarketPricing.Models
+{
+    public class ProductOnOffer : Product
+    {
+        public virtual int Quantity { get; set; }
+        public virtual decimal Price { get; set; }
+    }
+}
